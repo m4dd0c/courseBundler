a6d2f3a67e42aca3b0f994e61a6e2065395d7ee1
Manish Suthar <manishsuthar078@gmail.com>
2024-08-28 14:33:25 +0530

    server url added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,7 +6,7 @@ import adminReducer from "./reducers/adminReducers.js";
 import globalReducer from "./reducers/globalReducer.js";
 import axios from "axios";
 
-export const server = "http://localhost:9000/api/v1";
+export const server = "https://coursebundler-9z25.onrender.com/api/v1";
 
 export const instance = axios.create({
   baseURL: server,
@@ -22,7 +22,7 @@ const store = configureStore({
     courseReducer,
     subscriptionReducer,
     adminReducer,
-    globalReducer
+    globalReducer,
   },
 });
 
